3a07cd7b802d5fcdf5aa17d36bc15255cb413b53
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-05-22 10:10:56 +0300

    Array Sınıfı Metodları

---
+++ ArraySinifiMetodları/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ArraySinifiMetodları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+            Console.WriteLine("***** Sırasız Dizi *****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("***** Sıralı Dizi *****");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Clear
+            Console.WriteLine("***** Array Clear Metodu *****");
+            Array.Clear(sayiDizisi, 2, 2); //2.indexten itibaren 2 elemanı sıfırlar.
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //Reverse
+            Console.WriteLine("***** Array Reverse Metodu *****");
+            Array.Reverse(sayiDizisi);//Diziyi ters çevirir.İlk elamanı son son elemanı ilk eleman yapar.
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //IndexOf
+            Console.WriteLine("***** Array IndexOf Metodu *****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));//23 elemanının indexini döner.
+            //Resize
+            Console.WriteLine("***** Array Resize Metodu *****");
+            Array.Resize<int>(ref sayiDizisi, 9);//Dizinin boyutunu 9 yapar ve elemanları korur.
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+        }
+    }
+}
